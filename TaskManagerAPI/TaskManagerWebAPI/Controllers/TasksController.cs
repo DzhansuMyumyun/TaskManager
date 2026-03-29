6b1017f6d85a4ccc5e1bc433e767fb516c33891a
Dzhansu Halim <dzhansu.halim@outlook.com>
2026-03-29 18:59:08 +0200

    Improved, Updated and Tested Web API

---
+++ TaskManagerAPI/TaskManagerWebAPI/Controllers/TasksController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        //IoC container = Inversion of Control
+
+        ITaskItemService _taskItemService;
+        public TasksController( ITaskItemService taskItemService)
+        {
+            _taskItemService = taskItemService;
+        }
+
+        [HttpGet]
+        public List<TaskItem> Get()
+        {
+            var result = _taskItemService.GetAll();
+            return result.Data;
+        }
+    }
+}
--- TaskManagerAPI/TaskManagerWebAPI/Program.cs
+++ TaskManagerAPI/TaskManagerWebAPI/Program.cs
@@ -8,15 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // 1. Add Database Support (SQL Server)
-builder.Services.AddDbContext<TaskManagerContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<TaskManagerContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // 2. Dependency Injection: Linking Interfaces to Classes
 // Data Access Layer
-builder.Services.AddScoped<ITaskDal, EfTaskItemDal>();
+builder.Services.AddSingleton<ITaskDal, EfTaskItemDal>();
 
 // Business Layer
-builder.Services.AddScoped<ITaskItemService, TaskItemManager>();
+builder.Services.AddSingleton<ITaskItemService, TaskItemManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -24,14 +24,16 @@ builder.Services.AddOpenApi();
 
 // 4. OpenAPI/Swagger Support
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // Most devs still prefer SwaggerGen for the UI
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();   // This creates the JSON spec
+    app.UseSwaggerUI(); // This creates the UI at /swagger
 }
 
 app.UseHttpsRedirection();
